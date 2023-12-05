a150fabfa91f46221831c7251a78f6fa18c6365d
RaulPPealez <raulppelaez@gmail.com>
2023-12-05 13:12:30 +0100

    Remove old code

--- torchmdnet/extensions/neighbors/neighbors_cuda.cu
+++ torchmdnet/extensions/neighbors/neighbors_cuda.cu
@@ -1,6 +1,7 @@
 /* Raul P. Pelaez 2023
    Connection between the neighbor CUDA implementations and the torch extension.
  */
+#include "neighbors_backwards.h"
 #include "neighbors_cuda_brute.cuh"
 #include "neighbors_cuda_cell.cuh"
 #include "neighbors_cuda_shared.cuh"
@@ -19,44 +20,6 @@ static auto call_forward_kernel(const std::string& kernel_name, const T&... args
     return Tensor();
 }
 
-// This is the autograd function that is called when the user calls get_neighbor_pairs.
-// It dispatches the required strategy for the forward function and implements the backward
-// function. The backward function is written in full pytorch so that it can be differentiated a
-// second time automatically via Autograd.
-class NeighborAutograd : public torch::autograd::Function<NeighborAutograd> {
-public:
-    static tensor_list forward(AutogradContext* ctx, const std::string& strategy,
-                               const Tensor& positions, const Tensor& batch,
-                               const Tensor& box_vectors, bool use_periodic,
-                               const Scalar& cutoff_lower, const Scalar& cutoff_upper,
-                               const Scalar& max_num_pairs, bool loop, bool include_transpose) {
-        Tensor neighbors, deltas, distances, i_curr_pair;
-        std::tie(neighbors, deltas, distances, i_curr_pair) =
-            call_forward_kernel(strategy, positions, batch, box_vectors, use_periodic, cutoff_lower,
-                                cutoff_upper, max_num_pairs, loop, include_transpose);
-        ctx->save_for_backward({neighbors, deltas, distances});
-        ctx->saved_data["num_atoms"] = positions.size(0);
-        return {neighbors, deltas, distances, i_curr_pair};
-    }
-
-    using Slice = torch::indexing::Slice;
-
-    static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
-        auto saved = ctx->get_saved_variables();
-        auto edge_index = saved[0];
-        auto edge_vec = saved[1];
-        auto edge_weight = saved[2];
-        auto num_atoms = ctx->saved_data["num_atoms"].toInt();
-        auto grad_edge_vec = grad_outputs[1];
-        auto grad_edge_weight = grad_outputs[2];
-        auto grad_positions = call_backward_kernel(edge_index, edge_vec, edge_weight, grad_edge_vec,
-                                                   grad_edge_weight, num_atoms);
-        Tensor ignore;
-        return {ignore, grad_positions, ignore, ignore, ignore, ignore,
-                ignore, ignore,         ignore, ignore, ignore};
-    }
-};
-
 TORCH_LIBRARY_IMPL(torchmdnet_extensions, AutogradCUDA, m) {
     m.impl("get_neighbor_pairs",
            [](const std::string& strategy, const Tensor& positions, const Tensor& batch,
@@ -67,10 +30,6 @@ TORCH_LIBRARY_IMPL(torchmdnet_extensions, AutogradCUDA, m) {
                if (positions.size(0) >= 32768 && strategy == "brute") {
                    final_strategy = "shared";
                }
-               // auto result = NeighborAutograd::apply(final_strategy, positions, batch,
-               // box_vectors,
-               //                                       use_periodic, cutoff_lower, cutoff_upper,
-               //                                       max_num_pairs, loop, include_transpose);
                auto result = call_forward_kernel(final_strategy, positions, batch, box_vectors,
                                                  use_periodic, cutoff_lower, cutoff_upper,
                                                  max_num_pairs, loop, include_transpose);

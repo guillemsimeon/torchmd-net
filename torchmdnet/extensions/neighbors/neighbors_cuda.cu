fe150725ef604b4e037ea903b0b6187cb97b4e65
RaulPPealez <raulppelaez@gmail.com>
2023-12-05 14:35:49 +0100

    Update neighbors cuda

--- torchmdnet/extensions/neighbors/neighbors_cuda.cu
+++ torchmdnet/extensions/neighbors/neighbors_cuda.cu
@@ -6,19 +6,6 @@
 #include "neighbors_cuda_cell.cuh"
 #include "neighbors_cuda_shared.cuh"
 #include <torch/extension.h>
-template <class... T>
-static auto call_forward_kernel(const std::string& kernel_name, const T&... args) {
-    if (kernel_name == "brute") {
-        return forward_brute(args...);
-    } else if (kernel_name == "cell") {
-        return forward_cell(args...);
-    } else if (kernel_name == "shared") {
-        return forward_shared(args...);
-    } else {
-        throw std::runtime_error("Unknown kernel name");
-    }
-    return Tensor();
-}
 
 TORCH_LIBRARY_IMPL(torchmdnet_extensions, AutogradCUDA, m) {
     m.impl("get_neighbor_pairs",
@@ -30,9 +17,16 @@ TORCH_LIBRARY_IMPL(torchmdnet_extensions, AutogradCUDA, m) {
                if (positions.size(0) >= 32768 && strategy == "brute") {
                    final_strategy = "shared";
                }
-               auto result = call_forward_kernel(final_strategy, positions, batch, box_vectors,
-                                                 use_periodic, cutoff_lower, cutoff_upper,
-                                                 max_num_pairs, loop, include_transpose);
-               return std::make_tuple(result[0], result[1], result[2], result[3]);
+               auto kernel = forward_brute;
+               if (final_strategy == "cell") {
+                   kernel = forward_cell;
+               } else if (final_strategy == "shared") {
+                   kernel = forward_shared;
+               } else if (final_strategy != "brute") {
+                   throw std::runtime_error("Unknown kernel name");
+               }
+               auto result = kernel(positions, batch, box_vectors, use_periodic, cutoff_lower,
+                                    cutoff_upper, max_num_pairs, loop, include_transpose);
+               return result;
            });
 }
